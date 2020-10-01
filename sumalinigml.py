56a2dc2ca8657fcb0c8c27952fa05f98e20e1259
angelsumalini <70747259+angelsumalini@users.noreply.github.com>
2020-10-01 18:36:38 +0530

    Add files via upload

---
+++ sumalinigml.py
@@ -0,0 +1,9 @@
+myeid="sumalini@gmail.com"
+mypaw="1234"
+
+eid=input("enter a email")
+paw=input("enter the password")
+if  myeid==eid and mypaw==paw:
+    print("logged in")
+else:
+    print("invalid credential")
