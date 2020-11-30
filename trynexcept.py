fec48e3cdcc050dc852182b9227a6f03fad47c63
angelsumalini <70747259+angelsumalini@users.noreply.github.com>
2020-11-30 15:29:38 +0530

    Add files via upload

---
+++ trynexcept.py
@@ -0,0 +1,18 @@
+##a=10
+##try:
+##    print(a+b)
+##except:
+##        print('hi')
+
+##a=10
+##try:
+##    print(a)
+##    print(a+b)
+##except NameError:
+##    print("avoiding naming")
+##except zeroDivisionError:
+##    print('can not divide with zero')
+##else:
+##    print('no error')
+##finally:
+##    print('done')
