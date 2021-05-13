dd28badb249e4b141b065cd4768e0bba77c1b86c
angelsumalini <70747259+angelsumalini@users.noreply.github.com>
2021-05-13 12:19:05 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,4 @@
+first_name=input('enter first name?')
+middle_name=input('enter middle name?')
+last_name=input('enter last name?')
+print('your initials are',first_name[0],middle_name[0],last_name[0])
